5443e0b88436a0a328c51ef00f74240850de38b3
chloepoon <chloepoonpph@gmail.com>
2023-11-22 23:15:23 +0800

    20231122 update

--- self-exercise/src/SE1122.java
+++ self-exercise/src/SE1122.java
@@ -43,6 +43,37 @@ public class SE1122 {
             System.out.println("false");
         }
         
-  }
+        // if buy >= 500 dollars -> 80% off
+        double totalAmount = 0.0d;
+        int amount = 300;
+        double discount = 0.8d; 
+
+        if (amount >= 500) {
+            totalAmount = amount * discount;
+        } else {
+            totalAmount = amount;
+        }
+        System.out.println("Total Amount = " + totalAmount);
+
+        // if buy > 600 -> 90% off, after discount still >=600 -> free shipping fee, otherwise $30 shipping fee
+
+        double totalAmount2 = 0.0d;
+        int amount2 = 1000;
+        int afterDiscount = 600;
+        double discount2 = 0.9d;
+        int shippingFee = 30;
+
+        if (amount2 > 600) {
+            totalAmount2 = amount2 * discount2;
+        } else {
+            totalAmount2 = amount2;
+        }
+
+        if (totalAmount2 <= afterDiscount) {
+            totalAmount2 = totalAmount2 + 30;
+        }
+        System.out.println("Total amount 2 =" + totalAmount2);
+
+    }
   
 }
